35b26d611eadacdffe81402a15c3aec42469e0ff
Owadd <142593595+Owadd@users.noreply.github.com>
2023-09-11 19:36:40 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// Reading Files Asynchronously
+const readline =  require('readline');
+const fs = require('fs');
+
+// fs.readFile('./Files/start.txt','utf-8',(error1,data1) =>{
+//     console.log(data1); // start.txt contains the word "input"
+//     fs.readFile(`./Files/${data1}.txt`,`utf-8`, (error2,data2) => {
+//         console.log(data2);
+//         fs.readFile(`./Files/append.txt`,`utf-8`,(error3,data3) => {
+//             console.log(data3);
+//             fs.writeFile(`./Files/output.txt`, `${data2}\n\n${data3}\n\nDate Created :${new Date}`), () => {
+//                 console.log('File written successfully');
+//             }
+//         })
+//     })
+//  } )
+
+// console.log('Reading File...');
+
+const http = require('http');
+
+//To create a server
+const server = http.createServer((request,response) => {
+    console.log('A new request has been received');
+});
+
+//To start a server
+server.listen(5500,'127.0.0.1', () => {
+    console.log('The server has started');
+})
